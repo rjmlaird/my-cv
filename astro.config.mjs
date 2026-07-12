cef650f5afe9d49fa770f61925f36f1944e67d69
rjmlaird <rjmlaird@gmail.com>
2026-07-12 20:34:31 +0100

    push

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://cv.rjmlaird.co.uk',
   output: 'static',
   compressHTML: true,
+  integrations: [sitemap()],
 });

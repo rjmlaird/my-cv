e49080f37233e6cd30f3c74efbaadebfcb5e43fe
rjmlaird <rjmlaird@gmail.com>
2026-07-17 01:38:17 +0100

    icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://cv.rjmlaird.co.uk',
   output: 'static',
   compressHTML: true,
-  integrations: [sitemap()],
+  integrations: [sitemap(),icon()],
 });
